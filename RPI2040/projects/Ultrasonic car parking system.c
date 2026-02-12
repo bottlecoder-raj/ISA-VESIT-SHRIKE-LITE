d4dadc1ddf7ab29bd41fa5555b78eb95e864dc8b
Raj Narayan <hazrarajnarayan55@gmail.com>
2026-02-12 14:44:33 +0530

    Refactor ultrasonic sensor configuration and logic
    Updated pin definitions and added support for additional ultrasonic sensors. Adjusted logic for parking slot detection using 6 sensors

--- RPI2040/projects/Ultrasonic car parking system.c
+++ RPI2040/projects/Ultrasonic car parking system.c
@@ -1,8 +1,15 @@
-#define TRIG1 6
-#define ECHO1 7
-#define TRIG2 17
-#define ECHO2 18
-#define LED   4
+#define TRIG2 5
+#define ECHO2 6
+#define TRIG1 7
+#define ECHO1 8
+#define TRIG4 9
+#define ECHO4 10
+#define TRIG5 11
+#define ECHO5 14
+#define TRIG6 28
+#define ECHO6 29
+#define TRIG3 26
+#define ECHO3 27
 
 void setup() {
   Serial.begin(115200);
@@ -13,17 +20,19 @@ void setup() {
   pinMode(TRIG2, OUTPUT);
   pinMode(ECHO2, INPUT);
 
-  pinMode(LED, OUTPUT);
+  pinMode(TRIG3, OUTPUT);
+  pinMode(ECHO3, INPUT);
 
-  pinMode(5, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(19, OUTPUT);
-  pinMode(16, OUTPUT);
+  pinMode(TRIG4, OUTPUT);
+  pinMode(ECHO4, INPUT);
 
-  digitalWrite(5, HIGH);
-  digitalWrite(8, LOW);
-  digitalWrite(16, HIGH);
-  digitalWrite(19, LOW);
+    pinMode(TRIG5, OUTPUT);
+  pinMode(ECHO5, INPUT);
+
+  pinMode(TRIG6, OUTPUT);
+  pinMode(ECHO6, INPUT);
+
+  
 }
 
 float readUltrasonic(int trigPin, int echoPin) {
@@ -46,23 +55,28 @@ float readUltrasonic(int trigPin, int echoPin) {
 void loop() {
 
   float distance1 = readUltrasonic(TRIG1, ECHO1);
+  (distance1<=10)?Serial.print("SLOT 1:PARKED   "):Serial.print("SLOT 1:FREE  ");
   delay(50);   // small gap to avoid interference
 
   float distance2 = readUltrasonic(TRIG2, ECHO2);
+  (distance2<=10)?Serial.print("SLOT 2:PARKED  "):Serial.print("SLOT 2:FREE  ");
+  delay(50);
+  float distance3 = readUltrasonic(TRIG3, ECHO3);
+  (distance3<=10)?Serial.print("SLOT 2:PARKED   "):Serial.print("SLOT 3:FREE   ");
+  delay(50);   // small gap to avoid interference
 
-  Serial.print("SLOT 1: ");
-  Serial.print(distance1);
-  Serial.print(" cm   ");
+  float distance4 = readUltrasonic(TRIG4, ECHO4);
+  (distance4<=10)?Serial.print("SLOT 4:PARKED  "):Serial.print("SLOT 4:FREE   ");
+   delay(50);
+  float distance5 = readUltrasonic(TRIG5, ECHO5);
+  (distance5<=10)?Serial.print("SLOT 5:PARKED   "):Serial.print("SLOT 5:FREE  ");
+  delay(50);   // small gap to avoid interference5
+  float distance6 = readUltrasonic(TRIG6, ECHO6);
 
-  Serial.print("SLOT 2: ");
-  Serial.print(distance2);
-  Serial.println(" cm");
+  (distance6<=10)?Serial.print("SLOT 6:PARKED   "):Serial.print("SLOT 6:FREE   ");
+  delay(50); 
+  Serial.println();
+  
+delay(500);
 
-  if (distance1 <= 5 || distance2 <= 5) {
-    digitalWrite(LED, HIGH);
-  } else {
-    digitalWrite(LED, LOW);
-  }
-
-  delay(200);
 }
